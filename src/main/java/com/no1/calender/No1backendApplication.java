a7ff3d819d7e728aa4ca6a667f5a4b5883d77d25
jmk307 <jimin112688@gmail.com>
2022-08-20 01:27:34 +0900

    feat : 초기 프로젝트 설정

---
+++ src/main/java/com/no1/calender/No1backendApplication.java
@@ -0,0 +1,13 @@
+package com.no1.calender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class No1backendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(No1backendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/no1/calender/No1backendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.no1.calender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class No1backendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
