130ef55e25c30815493b28bf3e7b07a26f602182
jmk307 <jimin112688@gmail.com>
2022-08-20 17:06:00 +0900

    Update No1backendApplication.java

--- src/main/java/com/no1/calender/No1backendApplication.java
+++ src/main/java/com/no1/calender/No1backendApplication.java
@@ -2,12 +2,18 @@ package com.no1.calender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class No1backendApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(No1backendApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(No1backendApplication.class, args);
